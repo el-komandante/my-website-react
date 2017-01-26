0a8eb41084142ca8d110c7808459185f376e2d46
Rudy <rudydeberry@hotmail.com>
2017-01-26 11:07:02 -0600

    added fifa project webm

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,11 +3,19 @@ import React, { Component } from 'react'
 export default class Project extends Component {
   render() {
     return (
-      <div className="project">
+      <div className="project project-container">
         <a href={ this.props.link } target="_blank">
           <figure>
               <div className="project-image" data-content={ this.props.title }>
-                <img className="project" src={ this.props.image } />
+                {
+                  this.props.media === 'image'
+                  ? <img className="project" src={ this.props.src } />
+                  : (
+                      <video autoPlay loop className="project-video">
+                        <source src={ this.props.src } type="video/webm" />
+                      </video>
+                    )
+                }
               </div>
           </figure>
         </a>
